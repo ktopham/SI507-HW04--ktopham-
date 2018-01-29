81fd52a6e13ca3962ac3d7a4834d6fe3581c1586
Kate Topham <ktopham@umich.edu>
2018-01-29 17:11:11 -0500

    added while loop, checks for question and quit

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,10 @@
 def ask_question():
     question = input("What is your question?  ")
+    return question
 
-    
+while True:
+    q = ask_question()
+    if q == "quit":
+        break
+    elif q[-1] != "?":
+        print("Sorry, I only answer questions.")
